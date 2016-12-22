729a2c81e70c90c7f5525ad2f61a15dde56dda64
Axel Bruneaux <gazhrot@gmail.com>
2016-12-22 11:19:46 +0100

    update redirect route and routing

--- blog_5.2/app/Http/routes.php
+++ blog_5.2/app/Http/routes.php
@@ -10,16 +10,18 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/', function () {
-    return view('files/home');
-});
 
+// INDEX //
+Route::get('/', function () { return view('files/home'); });
+
+// LOGIN / REGISTER //
 Route::get('/register', 'Auth\AuthController@showRegistrationForm');
 Route::post('/register', 'Auth\AuthController@register');
 
 Route::get('/login', function () { return view('files/login'); });
 Route::post('/login', 'Auth\AuthController@login');
 Route::get('/logout', 'Auth\AuthController@logout');
+//
 
 Route::get('/edit', function () { return view('files/creatblog'); });
 Route::get('/office', function () { return view('files/office'); });
@@ -28,9 +30,7 @@ Route::get('/profil', function () { return view('files/profil'); });
 Route::get('/messagerie', function () { return view('files/messagerie '); });
 Route::get('/write', function () { return view('files/write '); });
 Route::get('/editcat', function () { return view('files/editcat '); });
-
 Route::get('/article', function () { return view('files/article'); });
-
 
 Route::post('/article', 'ArticleController@create');
 Route::post('/categorie', 'CategorieController@create');
